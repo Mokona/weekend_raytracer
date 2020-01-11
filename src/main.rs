c2fe562330a23ccd4513811ff8940537fca0c834
Sylvain Glaize <mokona@puupuu.org>
2020-01-11 16:04:22 +0100

    Add Ray constructor and use it for dummy sky rendering.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 mod color;
 mod ppm;
+mod ray;
+mod vector3;
 
 use color::Color;
+use ray::Ray;
+use vector3::Vector3;
+
+fn color(ray: Ray) -> Color {
+    let mut normalized_direction = ray.point_at_parameter(1.);
+    normalized_direction.normalize();
+
+    let t = (0.5 * (normalized_direction.y + 1.)) as f32;
+
+    let white = Color::new(255, 255, 255);
+    let blue = Color::new(127, 178, 255);
+
+    white.lerp(blue, t)
+}
 
 fn main() {
     let width = 200;
     let height = 100;
 
-    let output = ppm::get_file_content(width, height, |u: u32, v: u32| -> Color {
-        let r = u as f32 / width as f32;
-        let g = v as f32 / height as f32;
-        let b = 0.2f32;
+    let lower_left_corner = Vector3::from((-2., -1., -1.));
+    let horizontal = Vector3::from((4., 0., 0.));
+    let vertical = Vector3::from((0., 2., 0.));
+    let origin = Vector3::new();
 
-        Color::from((r, g, b))
+    let output = ppm::get_file_content(width, height, |x: u32, y: u32| -> Color {
+        let u = x as f64 / width as f64;
+        let v = y as f64 / height as f64;
+
+        let ray = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+
+        color(ray)
     });
 
     print!("{}", output);
--- src/ray.rs
+++ src/ray.rs
@@ -6,6 +6,10 @@ pub struct Ray {
 }
 
 impl Ray {
+    pub fn new(origin: Vector3, direction: Vector3) -> Self {
+        Ray { origin, direction }
+    }
+
     pub fn point_at_parameter(&self, t: f64) -> Vector3 {
         self.origin + self.direction * t
     }
@@ -20,7 +24,7 @@ mod tests {
     fn computes_point_at_parameter() {
         let origin = Vector3::from((0., 0., 0.));
         let direction = Vector3::from((0., 1., 0.));
-        let ray = Ray { origin, direction };
+        let ray = Ray::new(origin, direction);
 
         let result = Vector3::from((0., 4., 0.));
 
