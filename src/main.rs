ffd6bd20254a7265d8c290a3e04922105457abb9
Sylvain Glaize <mokona@puupuu.org>
2020-01-11 18:54:13 +0100

    Display a red unlit sphere.

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,32 @@ use color::Color;
 use ray::Ray;
 use vector3::Vector3;
 
+fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> bool {
+    let sphere_to_ray_origin = ray.origin - center;
+    let a = ray.direction.dot(&ray.direction);
+    let b = 2. * sphere_to_ray_origin.dot(&ray.direction);
+    let c = sphere_to_ray_origin.dot(&sphere_to_ray_origin) - radius * radius;
+    let discriminant = b * b - 4. * a * c;
+    discriminant > 0.
+}
+
 fn color(ray: Ray) -> Color {
-    let normalized_direction = ray.point_at_parameter(1.).normalized();
-    let t = (0.5 * (normalized_direction.y + 1.)) as f32;
+    if hit_sphere(Vector3::from((0., 0., -1.)), 0.5, &ray) {
+        Color::new(255, 0, 0)
+    } else {
+        let normalized_direction = ray.point_at_parameter(1.).normalized();
+        let t = (0.5 * (normalized_direction.y + 1.)) as f32;
 
-    let white = Color::new(255, 255, 255);
-    let blue = Color::new(127, 178, 255);
+        let white = Color::new(255, 255, 255);
+        let blue = Color::new(127, 178, 255);
 
-    white.lerp(blue, t)
+        white.lerp(blue, t)
+    }
 }
 
 fn main() {
-    let width = 200;
-    let height = 100;
+    let width = 400;
+    let height = 200;
 
     let lower_left_corner = Vector3::from((-2., -1., -1.));
     let horizontal = Vector3::from((4., 0., 0.));
