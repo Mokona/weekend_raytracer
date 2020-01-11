4ad7ec41a185037362ed04fc9c7d9c119df0957f
Sylvain Glaize <mokona@puupuu.org>
2020-01-11 20:42:35 +0100

    Display normals as colors on the sphere.

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,38 @@ use color::Color;
 use ray::Ray;
 use vector3::Vector3;
 
-fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: Vector3, radius: f64, ray: &Ray) -> Option<f64> {
     let sphere_to_ray_origin = ray.origin - center;
     let a = ray.direction.dot(&ray.direction);
     let b = 2. * sphere_to_ray_origin.dot(&ray.direction);
     let c = sphere_to_ray_origin.dot(&sphere_to_ray_origin) - radius * radius;
     let discriminant = b * b - 4. * a * c;
-    discriminant > 0.
+
+    if discriminant < 0. {
+        None
+    } else {
+        Some((-b - discriminant.sqrt()) / (2. * a))
+    }
 }
 
 fn color(ray: Ray) -> Color {
-    if hit_sphere(Vector3::from((0., 0., -1.)), 0.5, &ray) {
-        Color::new(255, 0, 0)
-    } else {
-        let normalized_direction = ray.point_at_parameter(1.).normalized();
-        let t = (0.5 * (normalized_direction.y + 1.)) as f32;
+    let sphere_center = Vector3::from((0., 0., -1.));
+    let hit_point = hit_sphere(sphere_center, 0.5, &ray);
+    match hit_point {
+        Some(t) => {
+            let normal = (ray.point_at_parameter(t) - sphere_center).normalized();
+            let half_normal = (normal + Vector3::from((1., 1., 1.))) * 0.5;
+            Color::from((half_normal.x, half_normal.y, half_normal.z))
+        }
+        None => {
+            let normalized_direction = ray.point_at_parameter(1.).normalized();
+            let t = (0.5 * (normalized_direction.y + 1.)) as f32;
 
-        let white = Color::new(255, 255, 255);
-        let blue = Color::new(127, 178, 255);
+            let white = Color::new(255, 255, 255);
+            let blue = Color::new(127, 178, 255);
 
-        white.lerp(blue, t)
+            white.lerp(blue, t)
+        }
     }
 }
 
