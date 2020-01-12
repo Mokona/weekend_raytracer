927b6e7daec38b66670098f0dd81e88f21507bef
Sylvain Glaize <mokona@puupuu.org>
2020-01-12 17:56:30 +0100

    Rename the ppm output format test with a sensible name.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,7 +1,7 @@
 use weekend_raytracer::{color, ppm};
 
 #[test]
-fn test_test() {
+fn test_ppm_format() {
     const EXPECTED: &str = "P3\n1 1\n255\n0 0 0\n";
     let ppm_string = ppm::get_file_content(1, 1, |_, _| color::Color::new(0, 0, 0));
     assert_eq!(EXPECTED, ppm_string);
